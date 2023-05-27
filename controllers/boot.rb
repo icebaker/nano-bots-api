7944cfc6ce5d1776470c568f70d6de61f6296411
icebaker <icebaker@proton.me>
2023-05-27 19:30:02 -0300

    fix rainbow

--- controllers/boot.rb
+++ controllers/boot.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require 'rainbow'
+
 require_relative '../components/log'
 require_relative '../components/ddos'
 
 module BootController
   def self.boot!
     DDOS.setup!
+    Rainbow.enabled = true
     Log.instance.logger.info("Starting server on port #{ENV.fetch('PORT')}")
   end
 end
