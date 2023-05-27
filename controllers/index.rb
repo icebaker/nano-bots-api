a4ebc1dc6c76daba280d53ec2099400053dceea1
icebaker <icebaker@proton.me>
2023-05-27 19:11:19 -0300

    bump

--- controllers/index.rb
+++ controllers/index.rb
@@ -4,7 +4,7 @@ module IndexController
   def self.handler
     {
       project: 'Nano Bots API',
-      version: '0.0.2',
+      version: '0.0.3',
       documentation: 'https://spec.nbots.io',
       'live-editor': 'https://clinic.nbots.io',
       github: 'https://github.com/icebaker/nano-bots-api'
