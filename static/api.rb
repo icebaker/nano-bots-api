e55867ca4bb49b19bf70a27830c036b86ac20adc
icebaker <icebaker@proton.me>
2023-06-04 16:31:33 -0300

    upgrading nano-bots

--- static/api.rb
+++ static/api.rb
@@ -2,7 +2,7 @@
 
 API = {
   project: 'Nano Bots API',
-  version: '0.1.0',
+  version: '0.1.1',
   documentation: 'https://spec.nbots.io',
   'live-editor': 'https://clinic.nbots.io',
   github: 'https://github.com/icebaker/nano-bots-api'
