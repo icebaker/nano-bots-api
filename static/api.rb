0505b29add1317b310cbdfd4d598a8ab7a9adbed
icebaker <icebaker@proton.me>
2023-12-03 09:38:43 -0300

    bump to 1.0.4

--- static/api.rb
+++ static/api.rb
@@ -2,7 +2,7 @@
 
 API = {
   project: 'Nano Bots API',
-  version: '1.0.3',
+  version: '1.0.4',
   documentation: 'https://spec.nbots.io',
   'live-editor': 'https://clinic.nbots.io',
   github: 'https://github.com/icebaker/nano-bots-api'
